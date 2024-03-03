8af7d77f5a6f907911628f0b10126cda4f4be458
Ivan-ISS <vanya5@inbox.ru>
2024-03-04 02:42:50 +0500

    project_weather-widget: update code and styles for weather for 5 days

--- src/Components/InfoWeatherFiveDays/index.jsx
+++ src/Components/InfoWeatherFiveDays/index.jsx
@@ -21,42 +21,45 @@ const InfoWeatherFiveDays = ({ list, city }) => {
                                         <div className={style.widget__dayWeek}>
                                                 <>
                                                     <div className={style.widget__date}>
-                                                        <div>{element.date.dayWeek}</div>
-                                                        <div>{element.date.currentDate}</div>
+                                                        <div className={style.widget__day}>{element.date.dayWeek}</div>
+                                                        <div className={style.widget__currentDate}>{element.date.currentDate}</div>
                                                     </div>
-                                                    {list.map((elementJ, index) => {
-                                                        return (
-                                                            <>
-                                                            {elementJ.date.currentDate === element.date.currentDate && (['6:00', '12:00', '18:00'].includes(elementJ.date.currentTime)) ?
-                                                                <div className={ `${style.widget__tableRow} ${elementJ.date.currentTime === '12:00' ? style.widget__tableRowLight : style.widget__tableRowDark}` }>
-                                                                    <div className={style.widget__time}>
-                                                                        {elementJ.date.currentTime}
+                                                    <div className={style.widget__line}>
+                                                        {list.map((elementJ, index) => {
+                                                            return (
+                                                                <>
+                                                                {elementJ.date.currentDate === element.date.currentDate && (['0:00', '15:00', '21:00'].includes(elementJ.date.currentTime)) ?
+                                                                    <div className={ `${style.widget__tableRow} ${elementJ.date.currentTime === '15:00' ? style.widget__tableRowLight : style.widget__tableRowDark}` }>
+                                                                        <div className={style.widget__time}>
+                                                                            {elementJ.date.currentTime}
+                                                                        </div>
+                                                                        <div className={style.widget__cloudinessImg}>
+                                                                            <img className={style.widget__cloudinessIcon} src={`https://openweathermap.org/img/wn/${elementJ.codeIcon}@2x.png`} title={element.cloudiness} alt="cloudiness" />
+                                                                        </div>
+                                                                        <div className={style.widget__cloudiness}>
+                                                                            {elementJ.cloudiness}
+                                                                        </div>
+                                                                        <div className={ `${elementJ.temperature > 0 ? style.widget__temperaturePlus : style.widget__temperatureMinus} ${style.widget__temperatureCurrent}` }>
+                                                                            {elementJ.temperature}&#176;
+                                                                        </div>
+                                                                        <div className={style.widget__pressure}>
+                                                                            {elementJ.pressure + " мм рт.ст."}
+                                                                        </div>
+                                                                        <div className={style.widget__humidity}>
+                                                                            {elementJ.humidity + "%"}
+                                                                        </div>
+                                                                        <div className={style.widget__wind}>
+                                                                            {Math.round(elementJ.wind) + " м/с"}
+                                                                        </div>
                                                                     </div>
-                                                                    <div>
-                                                                        <img className={style.widget__cloudinessImg} src={`https://openweathermap.org/img/wn/${elementJ.codeIcon}@2x.png`} title={element.cloudiness} alt="cloudiness" />
-                                                                    </div>
-                                                                    <div className={ `${elementJ.temperature > 0 ? style.widget__temperaturePlus : style.widget__temperatureMinus} ${style.widget__temperatureCurrent}` }>
-                                                                        {elementJ.temperature}&#176;
-                                                                    </div>
-                                                                </div>
-                                                            : null}
-                                                            </>
+                                                                : null}
+                                                                </>
+                                                            )}
                                                         )}
-                                                    )}
+                                                    </div>
                                                 </>
                                         </div>
                                         : null}
-                                        {/* <div className={style.widget__tableRow}>
-                                            <div className={style.widget__time}>
-                                                {element.date.currentTime}
-                                            </div>
-                                            <div>
-                                                <img className={style.widget__cloudinessImg} src={`https://openweathermap.org/img/wn/${element.codeIcon}@2x.png`} title={element.cloudiness} alt="cloudiness" />
-                                            </div>
-                                            <div className={ `${element.temperature > 0 ? style.widget__temperaturePlus : style.widget__temperatureMinus} ${style.widget__temperatureCurrent}` }>
-                                                {element.temperature}&#176;
-                                            </div>
-                                        </div> */}
                                     </>
                                 )}
                             )}
